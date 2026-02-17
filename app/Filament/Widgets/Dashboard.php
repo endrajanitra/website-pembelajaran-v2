2b8c64d38d1cb69840c6d6f43cbe8f2fe952c0a9
Draa7 <janitraendra@gmail.com>
2026-02-17 21:48:33 +0700

    feat: widget kelas

--- app/Filament/Widgets/Dashboard.php
+++ app/Filament/Widgets/Dashboard.php
@@ -4,6 +4,7 @@ namespace App\Filament\Widgets;
 
 use App\Models\Siswa;
 use App\Models\Guru;
+use App\Models\Kelas;
 use Filament\Support\Enums\IconPosition;
 use Filament\Support\Icons\Heroicon;
 use Filament\Widgets\StatsOverviewWidget;
@@ -33,6 +34,19 @@ class Dashboard extends StatsOverviewWidget
                 ->descriptionIcon(Heroicon::ArrowUpLeft, IconPosition::Before)
                 ->chart(
                     Guru::selectRaw("MONTH(created_at) as month, COUNT(*) as count")
+                    ->whereYear("created_at", now()->year)
+                    ->groupBy("month")
+                    ->orderBy("month")
+                    ->pluck("count")
+                    ->toArray()
+                )
+                ->descriptionColor("success")
+                ->color("success"),
+            Stat::make("Total Kelas", Kelas::count())
+                ->description("Total kelas dalam satu bulan")  
+                ->descriptionIcon(Heroicon::ArrowUpLeft, IconPosition::Before)
+                ->chart(
+                    Kelas::selectRaw("MONTH(created_at) as month, COUNT(*) as count")
                     ->whereYear("created_at", now()->year)
                     ->groupBy("month")
                     ->orderBy("month")
