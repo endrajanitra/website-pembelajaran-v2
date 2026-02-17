115f53bc52ea5d6af590535feb00297d07e646f0
Draa7 <janitraendra@gmail.com>
2026-02-17 21:40:28 +0700

    feat: widget dashboard siswa

---
+++ app/Filament/Widgets/Dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Siswa;
+use Filament\Support\Enums\IconPosition;
+use Filament\Support\Icons\Heroicon;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class Dashboard extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make("Total Siswa", Siswa::count())
+                ->description("Total siswa dalam satu bulan")  
+                ->descriptionIcon(Heroicon::ArrowUpLeft, IconPosition::Before)
+                ->chart(
+                    Siswa::selectRaw("MONTH(created_at) as month, COUNT(*) as count")
+                    ->whereYear("created_at", now()->year)
+                    ->groupBy("month")
+                    ->orderBy("month")
+                    ->pluck("count")
+                    ->toArray()
+                )
+                ->descriptionColor("success")
+                ->color("success")
+        ];
+    }
+}
